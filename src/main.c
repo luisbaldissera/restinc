98735e015da9a89f0ba98423265a13b9f1aa8913
luisbaldissera <luis.baldissera26@gmail.com>
2024-12-09 10:41:47 +0100

    hascky way for find

--- src/main.c
+++ src/main.c
@@ -8,7 +8,6 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-
 // Static stack {{{
 #define STATIC_STACK_SIZE (1 << 16)
 struct SS {
@@ -68,7 +67,8 @@ struct LL *LL_insert(struct LL *linked_list, void *data);
  * it. If not found, it will return NULL and prev will be the last element. If
  * the found element is the first element, prev will be NULL.
  */
-struct LL *LL_find(struct LL *linked_list, LL_filtfunc filtfunc, struct LL **prev);
+struct LL *LL_find(struct LL *linked_list, LL_filtfunc filtfunc,
+                   struct LL **prev);
 /**
  * Free the linked list. Note that the data is not freed.
  */
@@ -128,9 +128,10 @@ int LL_array(struct LL *ll, void **array) {
 
 // Hash table {{{
 #define HASH_TABLE_BUCKET_SIZE (1 << 8)
+#define HASH_TABLE_KEY_MAX_SIZE (1 << 8)
 struct HT_entry {
-  char *key;
-  void *data;
+  char key[HASH_TABLE_KEY_MAX_SIZE];
+  void *value;
 };
 struct HT {
   struct LL *bucket[HASH_TABLE_BUCKET_SIZE];
@@ -160,12 +161,20 @@ void *HT_get(struct HT *hash_table, const char *key);
 /**
  * Free the hash table. Note that the data is not freed.
  */
-void HT_destroy(struct HT *hash_table);
-/**
- * Closure function for finding an entry by key in the internal linked list.
- * TODO: figure it out hot implementing this closure function.
- */
-LL_filtfunc _HT_find_by_key(const char *key);
+void HT_free(struct HT *hash_table);
+
+// WARNING: this is a hacky way to implement the closure function for finding
+// an entry by key. Not thread-safe.
+static char _HT_key[HASH_TABLE_KEY_MAX_SIZE];
+int _HT_key_filterfunc(void *entry) {
+  return strcmp(((struct HT_entry *)entry)->key, _HT_key) == 0;
+}
+
+LL_filtfunc _HT_entry_find_by_key_filtfunc(const char *key) {
+  strcpy(_HT_key, key);
+  return _HT_key_filterfunc;
+}
+// END WARNING
 
 int HT_hash(const char *key) {
   int hash = 0;
@@ -185,10 +194,10 @@ void HT_init(struct HT *hash_table) {
 
 void *HT_get(struct HT *hash_table, const char *key) {
   int hash = HT_hash(key);
-  struct LL *bucket = hash_table->bucket[hash];
-  struct LL *entry = LL_find(bucket, _HT_find_by_key(key), NULL);
+  struct LL *bucket = hash_table->bucket[hash],
+            *entry = LL_find(bucket, _HT_entry_find_by_key_filtfunc(key), NULL);
   if (entry) {
-    return entry->data;
+    return ((struct HT_entry *)entry->data)->value;
   }
   return NULL;
 }
@@ -199,18 +208,19 @@ void HT_set(struct HT *hash_table, const char *key, void *data) {
   struct HT_entry *entry;
   if (!ll_bucket) {
     entry = malloc(sizeof(struct HT_entry));
-    entry->key = strdup(key);
+    strcpy(entry->key, key);
     SS_push(&hash_table->_keys, entry->key);
-    entry->data = data;
+    entry->value = data;
     hash_table->bucket[hash] = LL_new(entry);
-  } else if ((ll_entry = LL_find(ll_bucket, _HT_find_by_key(key), &ll_prev))) {
+  } else if ((ll_entry = LL_find(ll_bucket, _HT_entry_find_by_key_filtfunc(key),
+                                 &ll_prev))) {
     entry = ll_entry->data;
-    entry->data = data;
+    entry->value = data;
   } else {
     entry = malloc(sizeof(struct HT_entry));
-    entry->key = strdup(key);
+    strcpy(entry->key, key);
     SS_push(&hash_table->_keys, entry->key);
-    entry->data = data;
+    entry->value = data;
     LL_insert(ll_prev, data);
   }
 }
@@ -223,7 +233,7 @@ int HT_keys(struct HT *hash_table, char **keys) {
   return i;
 }
 
-void HT_destroy(struct HT *hash_table) {
+void HT_free(struct HT *hash_table) {
   struct LL *ll_bucket, *ll_iter;
   struct HT_entry *entry;
   for (int i = 0; i < HASH_TABLE_BUCKET_SIZE; i++) {
